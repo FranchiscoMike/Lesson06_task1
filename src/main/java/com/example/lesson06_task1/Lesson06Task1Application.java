d5f9aec70c860ae9106034cc75b0502485a32af5
Jo'raqulov O'tkirjon <97774479+FranchiscoMike@users.noreply.github.com>
2022-04-16 13:38:35 +0500

    Initial commit

---
+++ src/main/java/com/example/lesson06_task1/Lesson06Task1Application.java
@@ -0,0 +1,13 @@
+package com.example.lesson06_task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson06Task1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson06Task1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lesson06_task1/Lesson06Task1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lesson06_task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson06Task1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
